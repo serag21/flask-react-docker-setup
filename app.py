dec804eac6666f9f02268a58024dea3aed0903c2
Serag Sorror <serag.sorror@gmail.com>
2024-11-17 19:11:34 -0800

    Initial Flask Setup

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/api/data', methods=['GET'])
+def get_data():
+    data = {
+        'message': 'Hello from Flask!'
+    }
+    return jsonify(data)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
+
